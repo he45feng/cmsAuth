1899c9786d9ea7d4d0a8a5618988f08eb673838a
wuwh <wuwh@hy.com>
2017-04-26 14:55:41 +0800

    tree 问题

--- src/module/function/index.vue
+++ src/module/function/index.vue
@@ -5,20 +5,19 @@
 
     <el-table :data="tableData" border style="width: 100%" class="umar-t10">
       <el-table-column fixed prop="fun_grp_name" label="名称" width="150"></el-table-column>
-      
       <el-table-column prop="create_user_name" label="创建人" width="120"></el-table-column>
       <el-table-column prop="fun_grp_description" label="备注" width="300"></el-table-column>
-      <el-table-column prop="create_date" label="创建日期" width="120"></el-table-column>
-      <el-table-column fixed="right" label="操作">
+      <el-table-column prop="create_date" label="创建日期" width="160"></el-table-column>
+      <el-table-column fixed="right" label="操作" >
       <template prop="fun_grp_id" scope="scope">
         <el-button  @click.native.prevent="edit(scope.$index, tableData)" type="text" size="small">编辑</el-button>
-        <el-button  @click.native.prevent="ShowDelDialog(scope.$index, tableData)" type="text" size="small">查看</el-button>
+        <el-button  @click.native.prevent="ShowRoleDialog(scope.$index)" type="text" size="small">查看功能列表</el-button>
         <el-button  @click.native.prevent="ShowDelDialog(scope.$index, tableData)" type="text" size="small">删除</el-button>
       </template>
       </el-table-column>
     </el-table>
 
-	<!--编辑添加dialog-->
+  <!--编辑添加dialog-->
     <el-dialog title="新增功能分类" size="large" v-model="dialogFormVisible">
       <el-form :model="form">
         <el-form-item label="名称：" :label-width="formLabelWidth">
@@ -26,23 +25,25 @@
         </el-form-item>
         <el-form-item label="功能：" :label-width="formLabelWidth">
           <el-tree 
-          	  class="choose-fun"
-			  :data="fun"
-			  :props="props"
-			  :load="loadNode"
-			  lazy
-			  show-checkbox
-			  @check-change="handleCheckChange">
-			</el-tree>
+              class="choose-fun"
+              ref="tree"
+        :data="fun"
+        :props="props"
+        node-key="fun_id"
+        show-checkbox
+        @check-change="handleCheckChange"
+        @node-expand="nodeexpand"
+        >
+      </el-tree>
         </el-form-item>
         <el-form-item label="备注：" :label-width="formLabelWidth">
-        	<el-input
-			  type="textarea"
-			  :rows="2"
-			  placeholder="请输入内容"
-			  v-model="form.fun_grp_descrition">
-			</el-input>
-    	</el-form-item>
+          <el-input
+        type="textarea"
+        :rows="2"
+        placeholder="请输入内容"
+        v-model="form.fun_grp_description">
+      </el-input>
+      </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
@@ -50,12 +51,12 @@
       </div>
     </el-dialog>
 
-	<!-- 删除提示 -->
+  <!-- 删除提示 -->
     <el-dialog title="提示" v-model="dialogVisible" size="tiny">
       <span>确定要删除吗？</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="del()">确 定</el-button>
+        <el-button type="primary" @click="del(tableData)">确 定</el-button>
       </span>
     </el-dialog>
 
@@ -63,8 +64,8 @@
 </template>
 <style>
   .choose-fun{
-  	max-height: 500px;
-  	overflow-y: scroll;
+    max-height: 500px;
+    overflow-y: scroll;
   }
 </style>
 <script>
@@ -73,8 +74,7 @@
     name:'activePublic',
     data() {
       return {
-        tableData: [{
-        }],
+        tableData: [],
         dialogTableVisible: false,
         dialogFormVisible: false,
         form: {
@@ -94,79 +94,123 @@
         note:'',
 
         //功能选择
-        fun: [{
-          'name': '邮件服务'
-        }, {
-          'name': '知识中心'
-        }],
+        fun: [],
         props: {
-          label: 'name',
-          children: 'zones'
+          id:'fun_id',
+          label: 'fun_name',
+          children: 'children'
         },
         count: 1
       }
     },
     mounted(){
       this.getFunctionsGrpList();
+      //this.handleNodeClick(this.fun);
     },
     methods: {
       add(){
         this.dialogType=0;
         this.dialogFormVisible=true;
+        this.form={
+          fun_grp_name : '',
+          fun_grp_description:''
+        };
+        var that=this;
+        var url=Cms.ip+'/rms/rmsService/queryFunctions';
+           Cms.axios(url,{},function(response){
+              if(response.status==200){
+                that.fun=response.data;
+                 that.$refs.tree.setCheckedKeys([]);
+              }
+            });
       },
       edit(idx,data){
         this.dialogType=1;
         this.indexidx=idx;
         this.dialogFormVisible = true;
         this.form={
-          name : data[idx].name
-        }
+          fun_grp_name : data[idx].fun_grp_name,
+          fun_grp_description:data[idx].fun_grp_description
+        };
+        var that=this;
+        let url=Cms.ip+'/rms/rmsService/queryFunctions';
+            Cms.axios(url,{},function(res){ 
+            if(res.status==200){
+                  that.fun=res.data;
+                  let url=Cms.ip+'/rms/rmsService/queryFunctionsRight';
+                  Cms.axios(url,{},function(subres){
+                      let arr=[];
+                      for(let i=0;i<subres.data.length;i++){
+                          arr.push(subres.data[i].fun_id);
+                      }
+                      that.$refs.tree.setCheckedKeys(arr);
+                  });
+              }
+         });
       },
       ShowDelDialog(idx,data){
         this.indexidx=idx;
         this.dialogVisible = true;
       },
-      del(){
+      ShowRoleDialog(idx){
+        this.indexidx=idx;
+        //this.dialogVisible=true;
+      },
+      del(data){
         var url=Cms.ip+'/rms/rmsService/deleteFunctionsGroup';
-        var param={fun_grp_id:this.tableData[this.indexidx].fun_grp_id};
-        let that=this;
+        console.log(data[this.indexidx].fun_grp_id);
+        var param={fun_grp_id:data[this.indexidx].fun_grp_id};
+        var that=this;
         Cms.axios(url+'?'+JSON.stringify(param),{},function(res){
           if(res.status==200){
             that.tableData.splice(that.indexidx,1);
           }
         });
+        this.dialogVisible=false;
       },
       submit(){
+        let checknodes=this.$refs.tree.getCheckedNodes();
         if(this.dialogType==0){
           var url=Cms.ip+'/rms/rmsService/addFunctionsGroup';
           var param={
             fun_grp_name:this.form.fun_grp_name,
-            fun_grp_descrition:this.form.fun_grp_descrition
+            fun_grp_description:this.form.fun_grp_description
           };
           var that=this;
           Cms.axios(url+'?'+JSON.stringify(param),{},function(res){
             if(res.status==200){
               that.tableData.unshift(res.data);
+              url=Cms.ip+'/rms/rmsService/addFunctionsRight';
+              for(let i=0;i<checknodes.length;i++){
+              let subparam={
+                fun_id:checknodes[i].fun_id,
+                fun_grp_id :res.data.fun_grp_id
+              };
+              Cms.axios(url+'?'+JSON.stringify(subparam),{},function(subres){
+                
+              });
+            }
             }
           });
-          this.dialogFormVisible=false;
+          
         }
         else if(this.dialogType==1){
           var url=Cms.ip+'/rms/rmsService/updateFunctionsGroup';
           var param={
             fun_grp_id:this.tableData[this.indexidx].fun_grp_id,
             fun_grp_name:this.form.fun_grp_name,
-            fun_grp_descritio:this.form.fun_grp_descrition
+            fun_grp_description:this.form.fun_grp_description
           }
           var that=this;
           Cms.axios(url+'?'+JSON.stringify(param),{},function(res){
             if(res.status==200){
               that.tableData[that.indexidx].fun_grp_name=param.fun_grp_name;
-              that.tableData[that.indexidx].fun_grp_descrition=param.fun_grp_descrition;
+              that.tableData[that.indexidx].fun_grp_description=param.fun_grp_description;
             }
           });
 
         }
+        this.dialogFormVisible=false;
       },
       getFunctionsGrpList(){
         var that=this;
@@ -179,18 +223,26 @@
                 }
               }
             });
-      },
+      },              
       //功能选择开始
       handleCheckChange(data, checked, indeterminate) {
-        console.log(data, checked, indeterminate);
+        //console.log(data, checked, indeterminate);
+        //console.log(this.$refs.tree.getCheckedNodes());
       },
       handleNodeClick(data) {
-        console.log(data);
       },
       loadNode(node, resolve) {
+        console.log('已经触发');
         if (node.level === 0) {
-          return resolve([{ name: '邮件服务' }, { name: '知识中心' }]);
+          var url=Cms.ip+'/rms/rmsService/queryFunctions';
+           Cms.axios(url,{},function(response){
+              if(response.status==200){
+                let funs=response.data;
+                return resolve(funs);
+              }
+            });
         }
+        
         if (node.level > 3) return resolve([]);
 
         var hasChild;
@@ -201,24 +253,15 @@
         } else {
           hasChild = Math.random() > 0.5;
         }
-
         setTimeout(() => {
           var data;
-          if (hasChild) {
-            data = [{
-              name: '邮件服务' + this.count++
-            }, {
-              name: '邮件服务' + this.count++
-            }];
-          } else {
-            data = [];
-          }
-
+          data = [];
           resolve(data);
-        }, 500);
-      }
-      //功能选择结束
 
+          }, 500);
+      },
+      //功能选择结束
+    
     }
   }
 </script> 
